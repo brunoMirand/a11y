5c1b09dee93404a6077c53b0758d92095808e50e
giorgio <gegiorgio1991@gmail.com>
2018-07-31 11:11:01 -0300

    ajust name file

--- js/button-a11y.js
+++ js/button-a11y.js
@@ -1,12 +1,16 @@
 function fonte(e) {
 	var elemento = $(".acessibilidade");
 	var fonte = parseInt(elemento.css('font-size'));
+	var rede = $("#btnFacebook");
+
+
 	e == 'a' ? fonte++ : fonte--;
-	if(fonte < 17 && fonte > 10){
+	if(fonte < 16 && fonte > 10){
 		elemento.css("fontSize", fonte);
 		if(fonte > 14){
 			elemento.css("height", 195);
 			elemento.css("padding",2);
+			rede.css("size",fonte);
 		}else{
 			elemento.css("height", 185);
 		}
