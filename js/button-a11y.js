5b0ac6ca7ed7c61a0855c1f32f26a66b56668345
giorgio <gegiorgio1991@gmail.com>
2018-08-02 17:02:57 -0300

    ajuste de cores do modo contraste

--- js/button-a11y.js
+++ js/button-a11y.js
@@ -2,7 +2,6 @@ function fonte(e) {
 	var elemento = $(".acessibilidade");
 	var fonte = parseInt(elemento.css('font-size'));
 	var bannerResp = $(".bannerResponsive");
-
 	var redesSociais = $(".dadosRedesSociais");
 
 	e == 'a' ? fonte++ : fonte=12;
@@ -15,47 +14,38 @@ function fonte(e) {
 			bannerResp.css('height', 75);
 			redesSociais.css('margin-top',20);
 			redesSociais.css('margin-right',0);
-	}
+		}
 		if(fonte > 14){
 			elemento.css("height", 195);
-			elemento.css("padding",2);
-			
+			elemento.css("padding",2);		
 		}else{
 			elemento.css("height", 185);
 		}
-		console.log(fonte);
 	}
 }
 
-
-
 function contraste(){
-	//var div = $("div");
-	var body = $("body");
-	var link = $("a");
-	var button = $(".btnLogin");
-	var banner = $(".imgBanner");
-	var topBar = $(".topBar");
+	var topBar = $(".topBar, .bannerDfp");
 	var containerTopoMenu = $('.containerTopoMenu');
-	var h = $('h2');
-
+	var body = $("body");
+	var link = $("a, h3, .bottomBar p");
+	var button = $(".btnLogin");
+	var subtitulo = $('h2');
+	var texto = $('.texto');
+	var textoEmpresas = $('p');	
+	var group = $('.bottomParceiros ul li span, .toggleListas span, .groupTitle b, .branco, i');
+	var footer = $('.bottomBarContainer,.navContainer,.titleFooter');
 	var content = $('.content');
-
-	content.css("color",'#fff');
-	content.css("background",'#111');
-
-
-	h.css("color",'#fff');
-
 	
-	containerTopoMenu.css("background",'#111');
-	button.css("background",'#000');
+	textoEmpresas.css('color',"#000");
+	group.css("color", '#fff');
+	footer.css("background", '#111');	
 	topBar.css("background",'#000');
+	texto.css("background", '#111');
+	containerTopoMenu.css("background",'#111');
 	body.css("background",'#000');
-	
 	link.css("color",'#fff');
-
-	
-
-
+	subtitulo.css("color",'#fff');
+	button.css("background",'#000');
+	content.css({'color':'#fff','background': '#111'});
 }
