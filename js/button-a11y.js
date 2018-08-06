2d78c268d9e1b6adbbfc7662993e38704433964c
giorgio <gegiorgio1991@gmail.com>
2018-08-06 17:19:33 -0300

    banner responsivo em fonte > 17 arrumado

--- js/button-a11y.js
+++ js/button-a11y.js
@@ -4,13 +4,21 @@ function fonte(e) {
 	var fonte = parseInt(elemento.css('font-size'));
 	var bannerResp = $(".bannerResponsive");
 	var redesSociais = $(".dadosRedesSociais");
+	var cadastrar = $(".buttonAzulFlat");
+	var content = $('.content');
+
+	var t = $('.bannerHumanizado .bannerResponsive p');
+
+	var boxVagas = $('.vagasRecentes');
 
 	e == 'a' ? fonte++ : fonte=12;
-	if(fonte < 16 && fonte > 11){
+	if(fonte < 20 && fonte > 11){
 		elemento.css("fontSize", fonte);
 		bannerResp.css('height', 120);
 		redesSociais.css('margin-right',840);
 		redesSociais.css('margin-top',0	);
+
+		console.log(fonte);
 		if (fonte == 12){
 			bannerResp.css('height', 75);
 			redesSociais.css('margin-top',20);
@@ -18,9 +26,26 @@ function fonte(e) {
 		}
 		if(fonte > 14){
 			elemento.css("height", 195);
-			elemento.css("padding",2);		
+			elemento.css("padding",2);
+			if(fonte > 16){
+				bannerResp.css('height', 150);
+				cadastrar.css('width',185);
+				cadastrar.css('margin-top','-35px');
+				cadastrar.css('margin-left',140);
+				t.css('margin-top', '30px');
+				t.css('margin-left', '-2px');
+				t.css('line-height', '32px');
+				t.css('text-align', 'left');
+				boxVagas.css("height",550);
+				boxVagas.css("background", "red");
+				content.css("height",225);
+				content.css("margin-top",'55px');
+				content.css("padding-bottom",10);
+				content.css("border","2px solid blue");
+			}		
 		}else{
 			elemento.css("height", 185);
+			cadastrar.css('width',145); 
 		}
 	}
 }
